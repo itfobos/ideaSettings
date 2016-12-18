70722778599fafc82335363c871aa2bcfd603776
Ilya Turukin <turukin@gmail.com>
2016-12-18 20:58:05 +0700

    Logging is added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,47 @@
+import cinderella.MedParser;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        initLog4j();
+
         MedParser.parseGroupsPage();
+
         System.out.println("OK");
     }
+
+    private static void initLog4j() throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        // then we have to create document-loader:
+        DocumentBuilder loader = factory.newDocumentBuilder();
+
+        loader.setEntityResolver(new EntityResolver() {
+            public InputSource resolveEntity(String publicId, String systemId)
+                    throws SAXException, IOException {
+                // Check for dtd ref
+                if (systemId.endsWith("org/apache/log4j/xml/log4j.dtd")) {
+                    // return the dtd from classpath
+                    return new InputSource(getClass().getClassLoader()
+                            .getResourceAsStream(
+                                    "org/apache/log4j/xml/log4j.dtd"));
+                }
+
+                // Resume normal flow
+                return null;
+            }
+        });
+
+        // loading a DOM-tree...
+        Document document = loader.parse(App.class.getResourceAsStream("/log4j.xml"));
+        DOMConfigurator.configure(document.getDocumentElement());
+    }
+
 }
--- src/main/java/cinderella/MedParser.java
+++ src/main/java/cinderella/MedParser.java
@@ -1,3 +1,6 @@
+package cinderella;
+
+import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -5,9 +8,11 @@ import java.io.IOException;
 
 @SuppressWarnings("WeakerAccess")
 public class MedParser {
+    private static final Logger logger = Logger.getLogger(MedParser.class);
     private static final String MED_URL = "http://www.ngmu.ru";
 
     public static void parseGroupsPage() throws IOException {
         Document document = Jsoup.connect(MED_URL + "/groups/").userAgent("Chrome").timeout(5000).get();
+        logger.debug("Ok");
     }
 }
